458bec43c386c4c19c5d150d4ca45b282349d047
Raag98 <raianurag9800@gmail.com>
2022-02-10 01:55:11 +0530

    Filter Animation Added

---
+++ Filter Animation/filter-animation/src/App.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+import Movie from './Movie';
+import Filter from './Filter';
+import { motion, AnimatePresence } from 'framer-motion';
+
+function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  const fetchPopular = async () => {
+    const data = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=5468739509f9bd6cf6266758d4e0c96f&language=en-US&page=1");
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  }
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  return (
+    <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="popular-movies">
+        <AnimatePresence>
+          {filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </AnimatePresence>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
